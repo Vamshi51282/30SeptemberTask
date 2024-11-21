ce43f43f9427dca70f644bef48dc51a83334445c
Vamshi51282 <135943611+Vamshi51282@users.noreply.github.com>
2024-11-21 17:26:58 +0530

    Add files via upload

---
+++ 30Septem.Task.js
@@ -0,0 +1,49 @@
+// task on 30
+//1
+for(a=1;a<=10;a++){   //using for loop
+    console.log(a);
+    
+}
+
+a=1
+while(a<=10){         //using while loop
+    console.log(a);
+    a++   
+}
+// 2
+for(a=1;a<=10;a++){
+    if(a%2==0){
+        console.log(a + "is even number"); 
+    }
+}
+//2
+b=1
+while(b<10){
+    if(b%2==0){
+        console.log(b + "is even number");
+    }
+    b++
+}
+//3
+
+for(a=1;a<=10;a++){
+    if(a%2==1){
+        console.log(a + "is odd number"); 
+    }
+}
+//3
+b=1
+while(b<10){
+    if(b%2==1){
+        console.log(b + "is odd number");
+    }
+    b++
+}
+//4
+i=35
+j=45
+if(i<=j){
+    console.log("grade D");
+}
+
+
